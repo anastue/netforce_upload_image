a2e3bbdce02eeb9b3c071d68f4a30262436f0647
anas.t@almacom.co.th <anas.t@almacom.co.th>
2016-10-13 11:34:23 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="netforce_upload_image",
+    version="3.1.0",
+    description="Module for upload many image to server",
+)
